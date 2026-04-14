7046baa2785f5113c2cc3b83e4efd8494c01ec50
wirapratamaa <wirapratama758@gmail.com>
2026-04-14 14:43:45 +0800

    enhancement : update about text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ export const About = () => {
             src="https://i.ibb.co.com/dwgXx5tB/Gemini-Generated-Image-eiifoleiifoleiif.png"
             alt=""
             loading="lazy"
-            className="rounded-full w-60 h-60 object-cover border-2 border-gray-200"
+            className="w-60 h-60 object-top object-cover rounded-full border-2 border-gray-200"
           />
         </div>
         <div className="w-full md:w-1/2">
@@ -24,13 +24,30 @@ export const About = () => {
           </div>
           <div className="flex">
             <span className="text-zinc-200 px-5 md:px-0">
+              Hi, I&apos;m Wira, I&apos;m a Frontend Developer with over 5 years
+              of experience in the technology industry, specializing in building
+              scalable, high-performance web applications. My core expertise
+              lies in React and Tailwind CSS, where I focus on creating clean,
+              responsive, and user-centric interfaces. Throughout my career,
+              I&apos;ve consistently delivered impactful solutions by
+              transforming complex requirements into intuitive digital
+              experiences. I take pride in writing maintainable code, optimizing
+              performance, and collaborating effectively with cross-functional
+              teams to bring products to life. One of my key strengths is my
+              ability to balance technical precision with user experience,
+              ensuring that every product I work on is not only functional but
+              also engaging and efficient. I&apos;m always eager to learn,
+              improve, and contribute to meaningful projects that push
+              boundaries and create real value.
+            </span>
+            {/* <span className="text-zinc-200 px-5 md:px-0">
               Hi, I&apos;m Wira, a passionate web developer with 3 years of
               experience. I love building beautiful and interactive websites
               that provide great user experiences. In my portfolio, you&apos;ll
               find a showcase of my projects, demonstrating my skills in
               front-end development using technologies like HTML, CSS,
               JavaScript, and React.
-            </span>
+            </span> */}
           </div>
           <div className="mt-4 px-5 md:px-0">
             <div className="flex flex-row text-zinc-200">
